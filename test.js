fbff4e7a01612d9fd51ee03fba396c462f61bf15
bibhu-otz <bib.mhnt@gmail.com>
2023-10-28 08:48:10 +0530

    JS Session-3

---
+++ test.js
@@ -0,0 +1,13 @@
+let apple = 5;
+let Apple = 9;
+
+let _name;
+let $_name;
+console.log(apple);
+console.log(Apple);
+console.log(typeof ("1" + 2));
+let ans = 6 / "3";
+console.log(ans);
+console.log(typeof ans);
+ans = "6" * "5";
+console.log(ans);
